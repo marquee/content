e39037c27baeb3108c3abe38fbc0a8af2ce8f6d8
Alec Perkins <alecperkins@gmail.com>
2013-09-04 16:24:22 -0400

    Porting content package from runtime project.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+        name                = 'droptype-content',
+        version             = '0.0.0',
+        description         = '',
+        long_description    = file('README.md').read(),
+        url                 = 'https://github.com/droptype/content',
+        author              = 'Alec Perkins',
+        author_email        = 'alec@droptype.com',
+        license             = 'UNLICENSE',
+        packages            = ['content'],
+        zip_safe            = False,
+        keywords            = '',
+        install_requires    = file('requirements.txt').read().splitlines(),
+        classifiers         = [
+            'Development Status :: 2 - Pre-Alpha',
+            'Intended Audience :: Developers',
+            'License :: Public Domain',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: Implementation :: CPython',
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        ],
+    )
